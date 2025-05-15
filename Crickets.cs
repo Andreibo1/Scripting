1415dfda3417ace12724775086a3c97d8b265687
Andreibo1 <bogdanandreidragan@gmail.com>
2025-05-15 15:19:17 +0200

    Add files via upload

--- Crickets.cs
+++ Crickets.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crickets.cs : MonoBehaviour
+public class Destroy.cs : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        print("Crickets!");
+        print("Destroy(gameObject)"); +
+           
     }
 
     // Update is called once per frame
