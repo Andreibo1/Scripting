3daa30058ba627ce330a0b7eecb92bbb1fbeac32
Andreibo1 <bogdanandreidragan@gmail.com>
2025-05-22 15:10:21 +0200

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        (newPosition.z) += 3;
+        speed*Time.DeltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
